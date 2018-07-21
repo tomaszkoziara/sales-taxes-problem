d674264f65bd537bf1d302acb0c010355c03c258
Tomasz Koziara <tomaszkoziara86@gmail.com>
2018-07-21 17:42:16 +0200

    initial commit to test travis

---
+++ src/calcular.ts
@@ -0,0 +1,7 @@
+export default class Calculator {
+
+    public add(a: number, b: number): number {
+        return a + b;
+    }
+
+}
---
+++ test/calculator.spec.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+
+describe("Calculator", () => {
+    describe("add", () => {
+        it("should return 3 when adding 1 and 2", () => {
+            chai.assert.isTrue(true);
+        });
+    });
+});
